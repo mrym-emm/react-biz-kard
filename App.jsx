a1447d56a7a2b4aa1ae1b812d51eb523f9ba4324
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-08-20 00:42:32 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import About from "./components/About.jsx";
+import Info from "./components/Info.jsx";
+import Interest from "./components/Interest.jsx";
+import Footer from "./components/Footer.jsx";
+
+export default function App() {
+  return (
+    <>
+      <main>
+        <Info />
+        <About />
+        <Interest />
+        <Footer />
+      </main>
+    </>
+  );
+}
